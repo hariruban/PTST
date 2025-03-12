5c20de514c5b2faf6150952c446e660404a0e47e
hariruban <123630498+hariruban@users.noreply.github.com>
2025-03-12 20:29:08 +0530

    Update app.py

--- web_dashboard/app.py
+++ web_dashboard/app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import json
+
+st.title("PentestAutomator Dashboard")
+
+with open("reports/results.json", "r") as f:
+    data = json.load(f)
+
+st.subheader("Target: " + data["target"])
+st.write("### Subdomains Found")
+st.write(data["subdomains"])
+st.write("### Open Ports")
+st.write(data["open_ports"])
+st.write("### OWASP ZAP Vulnerabilities")
+st.write(data["vulnerabilities"])
 
